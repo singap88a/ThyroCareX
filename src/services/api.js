bd5fc8d1b95e1d0201046c84368a3a77f2de84e9
Ahmed Yasser Singap <asingap135@gmail.com>
2026-05-13 00:58:29 +0300

    feat: initialize axios instance with request and response interceptors for API service

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? 'http://localhost:5153/api'
-  : 'http://localhost:5153/api';
+const API_URL = 'https://thyrocarex.runasp.net/api';
 
 const api = axios.create({
   baseURL: API_URL,
